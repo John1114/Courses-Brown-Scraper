2fed42089a8ff0025f3e75295d996be1ee108cd0
Matthew Grinberg <matthew_grinberg@brown.edu>
2022-01-04 16:50:15 -0500

    started the parser with a REGEX

---
+++ csvParser.py
@@ -0,0 +1,21 @@
+import csv
+import re
+
+def useRegex(input):
+    pattern = re.compile(r"[a-zA-Z]+\\[a-zA-Z]{2}[0-9]{5}")
+    return pattern.match(input, re.IGNORECASE)
+
+with open('courses.csv') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            temp = useRegex(row[6])
+            if temp != None:
+                print(temp)
+            line_count+=1
+
+
