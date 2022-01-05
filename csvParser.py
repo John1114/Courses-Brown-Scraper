9fe83be6b07cbf7e694a77b1c864f14ae288e86c
Matthew Grinberg <matthew_grinberg@brown.edu>
2022-01-04 20:01:44 -0500

    cleaned up

--- csvParser.py
+++ csvParser.py
@@ -1,9 +1,15 @@
 import csv
 import re
+import string
 
 
-def useRegex(input):
+def useRegexToFindPreReqs(input):
     pattern = re.compile(r"[a-zA-Z]+\\[a-zA-Z]{2}[0-9]{5}")
+    return pattern.search(input, re.IGNORECASE)
+
+
+def useRegexToFilterPreReqs(input):
+    pattern = re.compile('(.*?)')
     return pattern.search(input, re.IGNORECASE)
 
 
@@ -15,7 +21,7 @@ with open('courses.csv') as csv_file:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            temp = useRegex(row[6])
+            temp = useRegexToFindPreReqs(row[6])
             if temp is not None:
-                print(temp)
+                print(temp.group(0))
             line_count += 1
